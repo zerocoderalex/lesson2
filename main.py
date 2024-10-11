080f75f7844012224a76cca749394360835bcd3e
zerocoderalex <alex12449@mail.ru>
2024-10-11 23:20:20 +0500

    new code

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello, cat!')
+import requests
+import pprint
+
+params = {'q':'html'}
+
+response = requests.get("https://api.github.com/search/repositories", params=params)
+
+print(response.status_code)
+response_json = response.json()
+
+pprint.pprint(response_json)
