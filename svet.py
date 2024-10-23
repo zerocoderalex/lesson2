69b3591d4356028ac58ce0c612afccea56d2a616
zerocoderalex <alex12449@mail.ru>
2024-10-23 19:41:18 +0500

    pravra

--- svet.py
+++ svet.py
@@ -2,41 +2,44 @@ import csv
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from w3lib.url import parse_data_uri
 
-# Настройка веб-драйвера
-driver = webdriver.Chrome()  # Убедитесь, что у вас установлен ChromeDriver
 
-url = 'https://tomsk.hh.ru/vacancies/programmist'
-driver.get(url)
-# Дайте странице время загрузиться
-time.sleep(3)
-
-# Соберите данные
-vacancies = driver.find_elements(By.CLASS_NAME, 'vacancy-card--H8LvOiOGPll0jZvYpxIF')
-print(vacancies)
-
+driver = webdriver.Chrome()
 parsed_data = []
+url = 'https://www.divan.ru/kazan/category/svet"/'
+driver.get(url)
 
-for vacancy in vacancies:
+def append_parsed_data(url):
+    print("Start parsing")
+    driver.get(url)
+
+time.sleep(30)
+
+
+cvets = driver.find_elements(By.CLASS_NAME, "div._Ud0k")
+print(cvets)
+
+
+
+for cvet in cvets:
     try:
-        title = vacancy.find_element(By.CSS_SELECTOR, 'span.vacancy-name--SYbxrgpHgHedVTkgI_cA').text
-        salary = vacancy.find_element(By.CSS_SELECTOR, 'span.compensation-text--cCPBXayRjn5GuLFWhGTJ').text
-        link = vacancy.find_element(By.CSS_SELECTOR, 'a.bloko-link').get_attribute('href')
+        title = cvet.find_element(By.CSS_SELECTOR, "div.lsooF").find_element(By.TAG_NAME, 'span').text
+        price =cvet.find_element(By.CSS_SELECTOR, "div.q5Uds").find_element(By.TAG_NAME, "span").text
+        link = cvet.find_element(By.TAG_NAME, 'a').get_attribute("href")
     except:
         print("Ошибка")
         continue
 
-    parsed_data.append([title, salary, link])
+    parsed_data.append([title, price, link])
 
 
-        # Закройте браузер
+
 driver.quit()
 
 # Сохраните данные в CSV
-with open('hh.csv', 'w', newline='', encoding='utf-8') as file:
+with open('cvet.csv', 'w', newline='', encoding='utf-8') as file:
 
     writer = csv.writer(file)
-    writer.writerow(['Название','зарплата','ссылка'])
+    writer.writerow(['Название','цена','ссылка'])
 
     writer.writerows(parsed_data)
