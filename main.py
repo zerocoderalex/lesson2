dc449df8dce8db8e2fc5de44fa9e55c3fce2d282
zerocoderalex <alex12449@mail.ru>
2024-11-06 21:52:21 +0500

    flask

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return render_template('index1.html')
+def index():
+    return render_template('index.html')
+@app.route('/contacts/')
+def contacts():
+    return render_template('contacts.html')
+@app.route('/blog/')
+def blog():
+    return render_template('blog.html')
+
 
 if __name__ == '__main__':
     app.run()
